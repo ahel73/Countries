8eed33802adc5b4b1e57a01775511546fc98d644
Нагаев Алексей <nag495@yandex.ru>
2023-02-27 00:51:57 +0300

    фиксация проекта

---
+++ src/store/countries/countries-selectors.js
@@ -0,0 +1,10 @@
+export const selectCountriesInfo = (state) => ({
+    status: state.countries.status,
+    error: state.countries.error,
+    qty: state.countries.list.length,
+})
+
+export const selectAllCountries = (state) => state.countries.list;
+export const selectVisebleCountres = (state, { search = '', region = '' }) => {
+    return state.countries.list.filter(country => country.name.toLowerCase().includes(search.toLowerCase()) && country.region.includes(region));
+}
